484a22aff6bef9207303ad97cb83841720807e22
lqyunn <limqiaoyun058@gmail.com>
2026-08-15 11:17:51 +0800

    route3_v2.5

--- Cybertronics_R3.py
+++ Cybertronics_R3.py
@@ -34,16 +34,17 @@ def Route3(laura: Laura):
 
     """ Start your code here """
     laura.wall_square()
-    laura.gyro_lock_turn(RIGHT_DRIVE, -43)
+    laura.gyro_lock_turn(RIGHT_DRIVE, -43.5)
     laura.gyro_acc(70, 400, -43, 45, stop=False)
     laura.gyro_time(55, 700, -43)
     laura.adapter_motor_seconds(RIGHT_ADAPTER, -800, 1200)
+    laura.adapter_motor_seconds(LEFT_ADAPTER, 300, 2000, wait_complete=False)
     laura.encoder_time(-50, -95, 1700)
     laura.encoder_degree(65, 65, 30)
     laura.adapter_motor_seconds(RIGHT_ADAPTER, 600, 1000)
     laura.encoder_acc(-140, -120, 760, stop=False)
     wait(350)
-    laura.gyro_lock_turn(RIGHT_DRIVE, 0, True, 100, 0)
+    laura.gyro_lock_turn(LEFT_DRIVE, 0, True, 100, 0)
     
 
 
